ebe74c39d81c37d60d2ffa60a9576f8b10fbffe6
Elizabeth Stein <purplegumdropz@gmail.com>
2026-04-16 07:32:01 -0400

    chore: add next-sitemap, bundle-analyzer, deps

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import "./src/env.ts";
 import type { NextConfig } from "next";
 import { withAxiom } from "next-axiom";
 import { withSentryConfig } from "@sentry/nextjs";
+import withBundleAnalyzerInit from "@next/bundle-analyzer";
+const withBundleAnalyzer = withBundleAnalyzerInit({ enabled: process.env.ANALYZE === "true" });
+
 
 const nextConfig: NextConfig = {
   poweredByHeader: false,
@@ -16,11 +19,11 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default withSentryConfig(withAxiom(nextConfig), {
+export default withBundleAnalyzer(withSentryConfig(withAxiom(nextConfig), {
   org: process.env.SENTRY_ORG,
   project: process.env.SENTRY_PROJECT,
   silent: !process.env.CI,
   widenClientFileUpload: true,
   disableLogger: true,
   automaticVercelMonitors: true,
-});
+}));
